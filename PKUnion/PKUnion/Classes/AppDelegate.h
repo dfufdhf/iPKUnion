b9f0231cf80e69fe9749dc93e98d1be4e45eb816
dfufdhf <Pineapple@RoydeMacBook-Pro.local>
2014-08-07 10:59:43 +0800

    First of all

---
+++ PKUnion/PKUnion/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  PKUnion
+//
+//  Created by Roy Sun on 14-8-7.
+//  Copyright PKUnion Team 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
