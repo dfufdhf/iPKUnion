8d4ada804de14efe509fadd44a34a131db95f2c6
dfufdhf <Pineapple@RoydeMacBook-Pro.local>
2014-08-12 00:45:47 +0800

    New version with coredata

---
+++ PKUnion/PKUnion/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PKUnion
+//
+//  Created by Roy Sun on 14-8-7.
+//  Copyright (c) 2014年 PKUnion Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PKUnion/PKUnionTests/PKUnionTests.m
@@ -0,0 +1,34 @@
+//
+//  PKUnionTests.m
+//  PKUnionTests
+//
+//  Created by Roy Sun on 14-8-12.
+//  Copyright (c) 2014年 PKUnion Team. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PKUnionTests : XCTestCase
+
+@end
+
+@implementation PKUnionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
