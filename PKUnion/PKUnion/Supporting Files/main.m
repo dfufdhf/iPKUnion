f7e5bd2080321962da32622aae135a3df0dfafc9
dfufdhf <Pineapple@RoydeMacBook-Pro.local>
2014-08-12 20:47:24 +0800

    base version with cocos2d & core data

---
+++ PKUnion/PKUnion/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PKUnion
+//
+//  Created by Roy Sun on 14-8-12.
+//  Copyright PKUnion Team 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
